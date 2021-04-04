1df4f7a6bb27f735e27444729a8512897337c5b0
dsnunes07 <dsnunes@usp.br>
2021-04-04 10:22:02 -0300

    checking properties and types

--- javascript-oo/byte-bank/SistemaAutenticacao.js
+++ javascript-oo/byte-bank/SistemaAutenticacao.js
@@ -1,5 +1,16 @@
+/* Ser autenticável significa ter a propriedade senha */
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.senha == senha;
+  static login(autenticavel, senha) {
+    if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    // verifica se a chave "autenticar" existe dentro do objeto e se ela é uma função
+    return "autenticar" in autenticavel &&
+      autenticavel.autenticar instanceof Function;
   }
 }
--- javascript-oo/byte-bank/index.js
+++ javascript-oo/byte-bank/index.js
@@ -3,13 +3,14 @@ import { Gerente } from './funcionarios/Gerente.js';
 import { Diretor } from './funcionarios/Diretor.js';
 import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
-const cliente = new Cliente("Daniel", 11122233300);
+const cliente = new Cliente("Daniel", 11122233300, "12345");
 const diretor = new Diretor('Silvio', 10000, 12334312200);
 diretor.cadastrarSenha("123456");
 const gerente = new Gerente('Edson', 5000, 23412378900);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456");
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
-console.log(gerenteEstaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "12345");
+
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado);
