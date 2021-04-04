69c12e4461c7c3798e112da1b7b285f99a5aea63
dsnunes07 <dsnunes@usp.br>
2021-04-04 10:27:06 -0300

    ducky type

--- javascript-oo/byte-bank/SistemaAutenticacao.js
+++ javascript-oo/byte-bank/SistemaAutenticacao.js
@@ -1,4 +1,6 @@
 /* Ser autenticável significa ter a propriedade senha */
+/* Ducky type: identificar o tipo de objetos pelos seus métodos e propriedades
+(não preciso saber se o tipo é Pato, mas só se ele anda como pato e faz quack) */
 export class SistemaAutenticacao {
   static login(autenticavel, senha) {
     if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
