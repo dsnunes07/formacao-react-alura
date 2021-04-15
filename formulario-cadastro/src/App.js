03ec1edfafea9b9f32bac172fe849d3739e18de6
dsnunes07 <dsnunes@usp.br>
2021-04-15 10:03:30 -0300

    form with hooks

--- formulario-cadastro/src/App.js
+++ formulario-cadastro/src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
-import RegisterForm from './components/RegisterForm/RegisterForm';
+import { Container, Typography } from "@material-ui/core";
+import "./App.css";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import 'fontsource-roboto'
 
 function App() {
-  return (<RegisterForm />);
+  return (
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">Register yourself now</Typography>
+      <RegisterForm submitAction={submitForm}/>
+    </Container>
+  );
+}
+
+function submitForm(data) {
+  console.log(data)
 }
 
 export default App;
--- formulario-cadastro/src/components/RegisterForm/RegisterForm.jsx
+++ formulario-cadastro/src/components/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,90 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { FormControlLabel, Switch } from "@material-ui/core";
 
-function RegisterForm() {
+function RegisterForm(props) {
+  const [name, setName] = useState();
+  const [lastname, setLastName] = useState();
+  const [cpf, setcpf] = useState();
+  const [novidades, setnovidades] = useState(true);
+  const [promotions, setpromotions] = useState(true);
   return (
-    <form>
-      <h2>Register yourself now</h2>
-      <input type="text" />
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        props.submitAction({
+          name,
+          lastname,
+          cpf,
+          novidades,
+          promotions,
+        });
+      }}
+    >
+      <TextField
+        value={name}
+        onChange={(event) => {
+          let inputName = event.target.value;
+          setName(inputName);
+        }}
+        label="Name"
+        variant="outlined"
+        id="name"
+        margin="normal"
+        fullWidth={true}
+      />
+      <TextField
+        label="Last Name"
+        variant="outlined"
+        id="lastName"
+        fullWidth={true}
+        margin="normal"
+        onChange={(event) => {
+          setLastName(event.target.value);
+        }}
+      />
+      <TextField
+        label="CPF"
+        value={cpf}
+        variant="outlined"
+        id="taxpayerRegistry"
+        fullWidth={true}
+        margin="normal"
+        onChange={(event) => {
+          const cpf = event.target.value;
+          setcpf(cpf);
+        }}
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            color="primary"
+            name="promotions"
+            checked={promotions}
+            onChange={(event) => {
+              setpromotions(event.target.checked);
+            }}
+          />
+        }
+        label="Promotions?"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            color="secondary"
+            name="novidades"
+            checked={novidades}
+            onChange={(event) => {
+              setnovidades(event.target.checked);
+            }}
+          ></Switch>
+        }
+        label="Novidades"
+      ></FormControlLabel>
+      <Button type="submit" variant="contained" color="primary">
+        Register
+      </Button>
     </form>
   );
 }
